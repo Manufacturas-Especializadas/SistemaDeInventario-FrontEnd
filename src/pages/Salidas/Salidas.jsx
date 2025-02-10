48080f06c4d57af08dff58d3c5a8fdb4fee5c9b8
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-02-10 14:03:53 -0600

    Boton para exportar datos

--- src/pages/Salidas/Salidas.jsx
+++ src/pages/Salidas/Salidas.jsx
@@ -1,19 +1,50 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react"
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
 export default function Salidas(){
     const[salidas, setSalidas] = useState([]);
 
     useEffect(() =>{
         const fetchSalidas = async () =>{
-            const response = await fetch('https://app-mesa-sistemadeinventario-api-prod.azurewebsites.net/api/Salidas/ObtenerSalidas');
+            const response = await fetch('https://localhost:44314/api/Salidas/ObtenerSalidas');
             const data = await response.json();
             console.log("Datos recibidos de la API de salidas:", data);
             setSalidas(data);
         }
         fetchSalidas();
     }, []);
+
+    const handleExport = async () => {
+        try {
+            const url = "https://localhost:44314/api/Salidas/DescargarExcel";
+
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",                    
+                },
+                body: null
+            });
+
+            if(!response.ok){
+                throw new Error(`Error: ${response.status}`);                
+            }
+            const blob = await response.blob();
+
+            const urlBlob = window.URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = urlBlob;
+            link.setAttribute("download", "DatosExportadosSalidas.xlsx");
+            document.body.appendChild(link);
+            link.click();
+
+            link.remove();
+        } catch (error) {
+            console.log("Error al exportar el excel: ", error);
+        }
+    }
 
     const columns = [
         {
@@ -38,6 +69,16 @@ export default function Salidas(){
             <h1 className="text-center text-5xl uppercase font-bold mb-2">
                 Salidas
             </h1>
+
+            <div className="flex justify-end">
+                <Button 
+                    color="secondary" 
+                    startIcon={<CloudDownloadIcon/>}
+                    onClick={ handleExport }
+                >
+                    Descargar información
+                </Button>
+            </div>
 
             <Box sx={{ height: 400, width: '100%', background: "#fff" }}>
                 <DataGrid
