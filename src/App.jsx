51f61508674dd6964b0ebbe020bc5dbf12762d82
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-02-21 08:03:30 -0600

    Se agrego un nuevo componente al navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Entradas from "./pages/Entradas/Entradas";
 import Salidas from "./pages/Salidas/Salidas";
 import Almacen from "./pages/Entradas/Almacen";
 import StorageIcon from "@mui/icons-material/Storage";
+import InventoryIcon from '@mui/icons-material/Inventory';
 import Login from "./pages/authentication/Register";
 import Register from "./pages/authentication/Login";
+import { Corte } from "./pages/Linea 1/Corte";
 
 const navArrayLinks =[
     {
@@ -23,6 +25,11 @@ const navArrayLinks =[
         title: "Almacén",
         path: "/almacen",
         icon: <StorageIcon/>
+    },
+    {
+        title: "Línea 1",
+        path: "/linea-1",
+        icon: <InventoryIcon/>
     }
 ]
 
@@ -35,6 +42,7 @@ export default function App(){
                     <Route path="/" element={<Entradas/>}/>
                     <Route path="/salidas" element={<Salidas/>}/>
                     <Route path="/almacen" element={<Almacen/>}/>
+                    <Route path="/linea-1" element={<Corte/>}/>
                     {/* <Route path="/login" element={<Login/>}/>
                     <Route path="/register" element={<Register/>}/> */}
                 </Routes>
