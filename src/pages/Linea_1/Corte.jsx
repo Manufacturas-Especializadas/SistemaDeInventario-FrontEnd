fd7901f8426c015d3119f83b3cb45e0947196dd8
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-02-21 08:59:37 -0600

    Descargar excel

--- src/pages/Linea_1/Corte.jsx
+++ src/pages/Linea_1/Corte.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { toast, Toaster } from "sonner";
 import { DataGrid } from '@mui/x-data-grid';
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
 export const Corte = () => {
     const[corte, setCorte] = useState([]);
@@ -33,6 +34,37 @@ export const Corte = () => {
         fetchCorte()
     }, []);
 
+    const handleExport = async () => {
+        try{
+            const url = "https://localhost:44314/api/Corte/DescargarExcel";
+
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: null
+            });
+
+            if(!response.ok){
+                throw new Error(`Error: ${response.status}`);
+            }
+
+            const blob = await response.blob();
+
+            const urlBlob = window.URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = urlBlob;
+            link.setAttribute("download", "DatosExportadosCorte.xlsx");
+            document.body.appendChild(link);
+            link.click();
+
+            link.remove();
+        }catch(error){
+            console.log("Error al exportar el excel: ", error);
+        }        
+    };
+
     const columns = [
         {
             field: "numeroDeParte",
@@ -61,7 +93,18 @@ export const Corte = () => {
 
             <Toaster position="top-right" richColors/>
 
-            <Box sx={{height: 400, width: '100%', background: '#fff', marginTop: 5, boxShadow: 5}}>
+
+            <div className="flex justify-end mt-5">
+                <Button
+                    startIcon={<CloudDownloadIcon/>} 
+                    color="secondary"
+                    onClick={ handleExport }
+                >
+                    Descargar información
+                </Button>
+            </div>
+
+            <Box sx={{height: 400, width: '100%', background: '#fff', marginTop: 1, boxShadow: 5}}>
                 <DataGrid
                     rows={ corte }
                     columns={ columns }
